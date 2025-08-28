4fee0ae09322f2056535cc99efaafd57ea5529b0
AayushAtMars <yoaayush14@.com>
2025-08-28 23:14:16 +0530

    Day3

---
+++ 3_FlowControl/src/IfElse.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class IfElse {
+    public static void main(String[] args){
+//        if(condition){
+//        code
+//        }
+//        else{
+//        code
+//        }
+
+//        System.out.print("Enter the Number : ");
+//        Scanner sc = new Scanner(System.in);
+//        int num = sc.nextInt();
+//
+//        if(num%2==0){
+//            System.out.print("Even");
+//        }
+//        else {
+//            System.out.println("Odd");
+//        }
+
+//        else-if
+
+//        if(condition){
+//        code
+//        }
+//        else if(condition){
+//        code
+//        }
+
+//
+
+//        Ternary operators -> returns a result
+        int a=16;
+        String result = (a<12)? "Less Than 12":"Greater than 12";
+        System.out.print(result);
+    }
+}
